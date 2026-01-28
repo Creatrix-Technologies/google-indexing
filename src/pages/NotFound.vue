1a28ef7ea6b15c995830d5d859aa5384653d4111
Avishek kunwar <avishekkunwar.work@gmail.com>
2026-01-28 21:45:49 +0545

    login issue fixed

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -3,8 +3,7 @@
     <h1>404 - Page Not Found</h1>
     <p>The page you are looking for does not exist or the URL is incorrect.</p>
     <div class="button-group">
-    <router-link to="/login" class="btn">Login</router-link>
-    <router-link to="/dashboard" class="btn">Home</router-link>
+    <router-link to="/dashboard" class="btn">Go Back To Dashboard</router-link>
   </div>
   </div>
 </template>
